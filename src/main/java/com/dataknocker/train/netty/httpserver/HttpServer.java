8b675aeec2d7bc2185c3334ab0efdb9c0cef9210
wangzejie <wangzejie@meicai.cn>
2020-12-09 07:29:16 +0800

    init

---
+++ src/main/java/com/dataknocker/train/netty/httpserver/HttpServer.java
@@ -0,0 +1,21 @@
+package com.dataknocker.train.netty.httpserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class HttpServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class);
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
